2885efb5b64cb3ec607f8a02621e224b6a77ca68
VirginiaRamirez <47005953+VirginiaRamirez@users.noreply.github.com>
2019-03-03 16:50:52 -0600

    Se actualiza función de filtrado y ordenado

--- src/data.js
+++ src/data.js
@@ -1,8 +1,27 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
-  return 'example';
+window.dataLovers = {
+  pokeFilter: (pokeData, pokeType) => {
+      //console.log(pokeType)
+      const filterByType = pokeData.filter(pokeData => pokeData.type.includes(pokeType));
+      return filterByType;
+  },
+
+  pokeSort: (pokeData, sortBy) =>{
+    if (sortBy == "a-Z"){
+      return pokeData.sort((a, b) => (a.name > b.name ? 1 : b.name > a.name ? -1 : 0));
+    }
+    if (sortBy == "Z-a"){
+      return pokeData.sort((a, b) => (a.name > b.name ? -1 : b.name > a.name ? 1 : 0));
+    }
+    if (sortBy == "001-151"){
+      return pokeData.sort((a, b) => (a.num > b.num ? 1 : b.num > a.num ? -1 : 0));
+    }
+    if (sortBy == "151-001"){
+      return pokeData.sort((a, b) => (a.num > b.num ? -1 : b.num > a.num ? 1 : 0));
+    }
+  }
+
 };
 
-window.example = example;
--- src/main.js
+++ src/main.js
@@ -1,44 +1,100 @@
+const pantallaInicio =()=>{
+    document.getElementById ("inicio").style.display="none";
+    document.getElementById ("root").style.display="flex";
+    document.getElementById ("header").style.display="flex";
+    document.getElementById ("type-select").style.display="flex";
+    document.getElementById ("sort-by").style.display="flex";
+    document.getElementById ("imagenes").style.display="flex";
+    
+  };
+  document.getElementById("botonInicio").addEventListener("click", pantallaInicio);
+
 //Data 
 const pokeData = window.POKEMON.pokemon;
-
+let root = document.getElementById("root");
 /*  Función para crear un template literal
     o plantilla de cadena de texto para incrustar
     las propiedades de los objetos */
-function pokeTemplate(pokemon) {
+
+function showAllPokemons(pokemon) {
     /*  Retorna un div que contiene la imagen, número,
-        nombre y tipo de Pokemon */
-        
-    return `
-    <div class="poke-box">
-        <p id=poke-num>${pokemon.num}<span id="poke-name"> ${pokemon.name}</span></p>    
-        <img class="poke-img" src="${pokemon.img}">
-        <p id=poke-type>${pokemon.type.join(", ")}</span></p>
-        <div class="wrap-text" id="text-footer">
-            <p class="poke-weight">Peso: <span id="att">${pokemon.weight}</span></p>
-            <p class="poke-height">Altura: <span id="att">${pokemon.height}</span></p>
-            <p class="poke-egg">Huevo: <span id="att">${pokemon.egg}</span></p>
-            <p class="poke-weak">Debilidades:<br> <span id="att">${pokemon.weaknesses.join(", ")}</span></p>
+        nombre y tipo de Pokemon */    
+        return `
+        <div class="poke-box">
+            <p id=poke-num>${pokemon.num}<span id="poke-name"> ${pokemon.name}</span></p>
+            <img class="poke-img" src="${pokemon.img}">
+            <p id=poke-type>${pokemon.type.join(", ")}</span></p>
+            <div class="wrap-text" id="text-footer">
+                <class="poke-weight">Peso: <span id="att">${pokemon.weight}</span><br>
+                <class="poke-height">Altura: <span id="att">${pokemon.height}</span><br>
+                <class="poke-egg">Huevo: <span id="att">${pokemon.egg}</span><br>
+                <class="poke-candy_count">Conteo de caramelos: <span id="att">${pokemon.candy_count}</span><br>
+                <class="poke-candy">Caramelo: <span id="att">${pokemon.candy}</span><br>
+                <class="poke-height">Posibilidad de engendro: <span id="att">${pokemon.spawn_chance}</span><br>
+                
+                <class="poke-height">Tiempo de engendro: <span id="att">${pokemon.spawn_time}</span><br>
+                <class="poke-height">Multiplicadores: <span id="att">${pokemon.multipliers}</span><br>
+                <class="poke-weak">Debilidades:<br> <span id="att">${pokemon.weaknesses.join(", ")}</span><br>
+            </div>
         </div>
-    </div>
-    `;
-}
+        `;
+    }
+
+
 /*  Se manda a llamar al contenedor/div "" de html para depositar
     los template generados por la función pokeTemplate */
 document.getElementById('root').innerHTML = `
-    ${pokeData.map(pokeTemplate).join("")}
+    ${pokeData.map(showAllPokemons).join("")}
 `;
+
 //Selector
 let typeSelector = document.getElementById("type-select");
-let typeButtons = ["Grass", "Poison", "Fire", "Ice", "Flying", "Psychic", "Water", "Ground", "Rock", "Electric", "Bug", "Fighting", "Fairy", "Ghost", "Dark", "Steel", "Dragon", "Normal"];
-
-typeButtons.forEach(buttonType => {
+let typeOptions = ["Grass", "Poison", "Fire", "Ice", "Flying", "Psychic", "Water", "Ground", "Rock", "Electric", "Bug", "Fighting", "Ghost", "Dragon", "Normal"];
+typeOptions.forEach(optionType => {
     let options = document.createElement("option");
-    options.textContent = buttonType;
-    options.id = buttonType;
-    options.value = buttonType;
+    options.textContent = optionType;
+    options.id = optionType;
+    options.value = optionType;
     typeSelector.appendChild(options);
 });
 
-let select = document.getElementById("type-select");
-select.addEventListener('change', window.pokeLovers.pokeFilter(POKEMON.pokemon, pokeType.value));
+const filterByType = document.getElementById("type-select");
+filterByType.addEventListener("change", () => {
+    let pokeType =filterByType.value;
+    let filtered = window.dataLovers.pokeFilter(pokeData, pokeType);
+    pokeTemplate(filtered);
+});
 
+
+const sortByName = document.getElementById("sort-by");
+sortByName.addEventListener("change", () => {
+    let sortBy = sortByName.value;
+    let sorted = window.dataLovers.pokeSort(pokeData, sortBy);
+    pokeTemplate(sorted);
+});
+
+function pokeTemplate (filtered){
+    let result = "";
+    root.innerHTML = '';
+   filtered.forEach(pokemon => {
+       result = root.innerHTML +=`
+            <div class="poke-box">
+            <p id=poke-num>${pokemon.num}<span id="poke-name"> ${pokemon.name}</span></p>
+            <img class="poke-img" src="${pokemon.img}">
+            <p id=poke-type>${pokemon.type.join(", ")}</span></p>
+            <div class="wrap-text" id="text-footer">
+                <class="poke-weight">Peso: <span id="att">${pokemon.weight}</span><br>
+                <class="poke-height">Altura: <span id="att">${pokemon.height}</span><br>
+                <class="poke-egg">Huevo: <span id="att">${pokemon.egg}</span><br>
+                <class="poke-candy_count">Conteo de caramelos: <span id="att">${pokemon.candy_count}</span><br>
+                <class="poke-candy">Caramelo: <span id="att">${pokemon.candy}</span><br>
+                <class="poke-height">Posibilidad de engendro: <span id="att">${pokemon.spawn_chance}</span><br>
+                <class="poke-height">Tiempo de engendro: <span id="att">${pokemon.spawn_time}</span><br>
+                <class="poke-height">Multiplicadores: <span id="att">${pokemon.multipliers}</span><br>
+                <class="poke-weak">Debilidades:<br> <span id="att">${pokemon.weaknesses.join(", ")}</span><br>
+            `;
+     });
+     return result;
+}
+
+  
